77a7b2f0401a545d10e8f316e6e17d0d0caeaa44
Moksh Jain <moksh.jain@samespace.com>
2025-05-22 16:21:36 +0530

    debug

--- recording.go
+++ recording.go
@@ -285,9 +285,9 @@ func (r *Room) StopRecording() error {
 	}
 
 	// Merge channels and upload to S3
-	// if err := r.mergeAndUpload(session); err != nil {
-	// 	return err
-	// }
+	if err := r.mergeAndUpload(session); err != nil {
+		return err
+	}
 
 	r.recordingMu.Lock()
 	r.recordingSession = nil
